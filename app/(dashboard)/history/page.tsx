123bd0642d9ee72b6d2bfebafdd978a1aff07e87
ravinder9280 <ravinder92809@gmail.com>
2025-06-27 11:46:35 +0530

    fix

--- app/(dashboard)/history/page.tsx
+++ app/(dashboard)/history/page.tsx
@@ -7,7 +7,7 @@ const HistoryPage = async() => {
   return (
     <div className='flex flex-col gap-4'>
       <div className='flex flex-col gap-4'>
-        <h1 className='text-2xl font-bold'>History</h1>
+        <h1 className='text-2xl font-bold'>History see</h1>
         <p className='text-sm text-gray-500'>View your history of grammar corrections</p>
       </div>
       <div>
